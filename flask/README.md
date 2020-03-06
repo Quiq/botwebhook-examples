085db95896cbae95358247f5ec4a1d53fc048a14
Nick Benoit <nick.benoit14@gmail.com>
2020-03-06 15:52:23 -0700

    Add flask example

---
+++ flask/README.md
@@ -0,0 +1,23 @@
+# Basic Flask Webhook Example
+
+## Usage: 
+
+
+### Run server
+
+```
+$ pip install flask
+$ cd botwebhook-examples/flask
+$ flask run
+```
+
+### Enable public gateway
+
+If you are running your bot locally, you will need to make sure it is visible to the public internet. Usually we
+use something like [Ngrok](https://ngrok.com/ ) for that. It doesn't matter how you accomplish this, so long as your server is 
+publically accessible. 
+
+### Configure quiq bot
+
+
+
---
+++ flask/app.py
@@ -0,0 +1,67 @@
+## 
+# Basic Flask Webhook Example
+#
+# usage: 
+# `pip install flask`
+# `flask run`
+
+
+
+from flask import Flask, request
+from logging.config import dictConfig
+
+# Configure logger
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET","POST"])
+def hello_world():
+    ##
+    # Receive a webhook json payload: More details -> (https://support.goquiq.com/api/docs#tag/Bot-Webhook)
+    # {
+    #   "conversation": {
+    #     "id": <id>,
+    #      .... 
+    #   }
+    #   "derivedData": {
+    #     "lastCustomerMessage": {...}
+    #   }
+    ##
+    update = request.json
+    app.logger.info("Recieved webhook for conversation %s", update["conversation"]["id"])
+
+    ##
+    # Build a response object: More details -> (https://support.goquiq.com/api/docs#tag/Bot-Webhook)
+    ##
+    resp = {
+      "actions": [{ 
+        "action": "sendMessage",
+        "message": {
+          "default": {
+            "text": "Hello World!"
+          },
+          "overrides": {
+            "SMS": {"text": "Hello SMS User!"},
+          }
+        }
+      }
+      # ... other actions
+      ]
+    }
+
+    return resp
